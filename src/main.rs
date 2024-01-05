d39bad1c4b40fe732664098215de30f69a7228eb
s1f10210368 <s1f102103684@iniad.org>
2024-01-05 23:45:06 +0900

    wasmothello

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-   a + b 
+pub struct Othello {
+    board: [[i8; 8]; 8],
+    current_player: i8,
 }
 
-#[wasm_bindgen(start)]
-pub fn main() {
-    // WebAssemblyの初期化
+#[wasm_bindgen]
+impl Othello {
+
+    pub fn new() -> Othello {
+        let mut board = [[0; 8]; 8];
+        board[3][3] = 1;
+        board[4][4] = 1;
+        board[3][4] = -1; 
+        board[4][3] = -1;
+
+        Othello {
+            board,
+            current_player: 1,  
+        }
+    }
+
+    pub fn make_move(&mut self, x: usize, y: usize) {
+        // 石を置く処理
+    }
+
+    pub fn get_current_player(&self) -> i8 {
+        self.current_player
+    }
+
 }
+
+
+_
